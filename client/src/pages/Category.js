427913795e9124bc0416525c0f6c9e43b0e94cad
cvntrieu <cvntrieu@gmail.com>
2026-05-18 16:53:53 +0700

    little add

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -33,26 +33,26 @@ const Category = () => {
   };
 
   const products = [
-    { name: "Table Lamp", category: "lamps", rating: "★★★★★", price: "₹4926", originalPrice: "₹4926", image: "https://images.unsplash.com/photo-1517991104123-1d56a6e81ed9?w=600&auto=format&fit=crop&q=60", model: "/models/table_lamp.glb" },
-    { name: "Floor Lamp", category: "lamps", rating: "★★★★★", price: "₹6597", originalPrice: "₹8267", image: "https://images.unsplash.com/photo-1743578666060-49a1747d61df?w=600&auto=format&fit=crop&q=60", model: "/models/lamp.glb" },
-    { name: "Modern Lamp", category: "lamps", rating: "★★★★☆", price: "₹4926", originalPrice: "₹5762", image: "https://images.unsplash.com/photo-1729825128716-13221811859f?w=600&auto=format&fit=crop&q=60", model: "/models/modern_lamp.glb" },
-    { name: "Vintage Lamp", category: "lamps", rating: "★★★★★", price: "₹6597", originalPrice: "₹8267", image: "https://images.unsplash.com/photo-1667316636895-0346ca648046?w=600&auto=format&fit=crop&q=60", model: "/models/vintage_lamp.glb" },
-    { name: "Desk Lamp", category: "lamps", rating: "★★★★☆", price: "₹3297", originalPrice: "₹4092", image: "https://plus.unsplash.com/premium_photo-1685287731237-d119a3d95711?w=600&auto=format&fit=crop&q=60", model: "/models/new_table_lamp.glb" },
-    { name: "Modern Bed", category: "beds", rating: "★★★★★", price: "₹24967", originalPrice: "₹29152", image: "https://images.unsplash.com/photo-1688384452844-8364c3e2fc28?w=600&auto=format&fit=crop&q=60", model: "/models/modern_bed.glb" },
-    { name: "King Size Bed", category: "beds", rating: "★★★★★", price: "₹33317", originalPrice: "₹37492", image: "https://images.unsplash.com/photo-1592229505678-cf99a9908e03?w=600&auto=format&fit=crop&q=60", model: "/models/king_size.glb" },
-    { name: "Wooden Bed", category: "beds", rating: "★★★★★", price: "₹24967", originalPrice: "₹37492", image: "https://images.unsplash.com/photo-1688384452844-8364c3e2fc28?w=600&auto=format&fit=crop&q=60", model: "/models/wooden_bed.glb" },
-    { name: "Sleek Bed", category: "beds", rating: "★★★★★", price: "₹26637", originalPrice: "₹30812", image: "https://plus.unsplash.com/premium_photo-1670076515907-2736a3492f23?w=600&auto=format&fit=crop&q=60", model: "/models/sleek_bed.glb" },
-    { name: "Minimalist Bed", category: "beds", rating: "★★★★☆", price: "₹23297", originalPrice: "₹27472", image: "https://plus.unsplash.com/premium_photo-1671269943825-e45b177add8f?w=600&auto=format&fit=crop&q=60", model: "/models/minimalist_bed.glb" },
-    { name: "Leather Sofa", category: "sofas", rating: "★★★★★", price: "₹41667", originalPrice: "₹45842", image: "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?w=600&auto=format&fit=crop&q=60", model: "/models//chesterfield-sofa.glb" },
-    { name: "Modern Sofa", category: "sofas", rating: "★★★★★", price: "₹33317", originalPrice: "₹37492", image: "https://images.unsplash.com/photo-1698936061086-2bf99c7b9fc5?w=600&auto=format&fit=crop&q=60", model: "/models/low_poly_modern_sofa_free_model.glb" },
-    { name: "Velvet Sofa", category: "sofas", rating: "★★★★★", price: "₹38327", originalPrice: "₹41667", image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=600&auto=format&fit=crop&q=60", model: "/models/green_velvet_three_seatersofa.glb" },
-    { name: "Compact Sofa", category: "sofas", rating: "★★★★☆", price: "₹29167", originalPrice: "₹32482", image: "https://images.unsplash.com/photo-1647221598498-245f6ed6c720?w=600&auto=format&fit=crop&q=60", model: "/models/compact_sofa.glb" },
-    { name: "L-Shaped Sofa", category: "sofas", rating: "★★★★★", price: "₹50017", originalPrice: "₹54192", image: "https://plus.unsplash.com/premium_photo-1692130314358-30f911957d7f?w=600&auto=format&fit=crop&q=60", model: "/models/L-shape.glb" },
-    { name: "Coffee Table", category: "tables", rating: "★★★★★", price: "₹10767", originalPrice: "₹12442", image: "https://images.unsplash.com/photo-1461418559055-6f020c5a91e7?w=600&auto=format&fit=crop&q=60", model: "/models/bar_table.glb" },
-    { name: "Dining Table", category: "tables", rating: "★★★★★", price: "₹24967", originalPrice: "₹29152", image: "https://images.unsplash.com/photo-1656470176663-1f5ed664e539?w=600&auto=format&fit=crop&q=60", model: "/models/simple_dining_table.glb" },
-    { name: "Side Table", category: "tables", rating: "★★★★☆", price: "₹6597", originalPrice: "₹8267", image: "https://images.unsplash.com/photo-1494949385013-8b57482a0e4f?w=600&auto=format&fit=crop&q=60", model: "/models/ikea_lack_side_table_55x55x45.glb" },
-    { name: "Round Table", category: "tables", rating: "★★★★☆", price: "₹12442", originalPrice: "₹14112", image: "https://plus.unsplash.com/premium_photo-1670869816894-e020bc93b279?w=600&auto=format&fit=crop&q=60", model: "/models/round_table.glb" },
-    { name: "Glass Table", category: "tables", rating: "★★★★★", price: "₹16617", originalPrice: "₹19122", image: "https://images.unsplash.com/photo-1563146413-d915a569d6b1?w=600&auto=format&fit=crop&q=60", model: "/models/round_glass_table.glb" }
+    { name: "Table Lamp", category: "lamps", rating: "★★★★★", price: "VNĐ492600", originalPrice: "VNĐ492600", image: "https://images.unsplash.com/photo-1517991104123-1d56a6e81ed9?w=600&auto=format&fit=crop&q=60", model: "/models/table_lamp.glb" },
+    { name: "Floor Lamp", category: "lamps", rating: "★★★★★", price: "VNĐ659700", originalPrice: "VNĐ826700", image: "https://images.unsplash.com/photo-1743578666060-49a1747d61df?w=600&auto=format&fit=crop&q=60", model: "/models/lamp.glb" },
+    { name: "Modern Lamp", category: "lamps", rating: "★★★★☆", price: "VNĐ492600", originalPrice: "VNĐ576200", image: "https://images.unsplash.com/photo-1729825128716-13221811859f?w=600&auto=format&fit=crop&q=60", model: "/models/modern_lamp.glb" },
+    { name: "Vintage Lamp", category: "lamps", rating: "★★★★★", price: "VNĐ659700", originalPrice: "VNĐ826700", image: "https://images.unsplash.com/photo-1667316636895-0346ca648046?w=600&auto=format&fit=crop&q=60", model: "/models/vintage_lamp.glb" },
+    { name: "Desk Lamp", category: "lamps", rating: "★★★★☆", price: "VNĐ329700", originalPrice: "VNĐ409200", image: "https://plus.unsplash.com/premium_photo-1685287731237-d119a3d95711?w=600&auto=format&fit=crop&q=60", model: "/models/new_table_lamp.glb" },
+    { name: "Modern Bed", category: "beds", rating: "★★★★★", price: "VNĐ2496700", originalPrice: "VNĐ2915200", image: "https://images.unsplash.com/photo-1688384452844-8364c3e2fc28?w=600&auto=format&fit=crop&q=60", model: "/models/modern_bed.glb" },
+    { name: "King Size Bed", category: "beds", rating: "★★★★★", price: "VNĐ3331700", originalPrice: "VNĐ3749200", image: "https://images.unsplash.com/photo-1592229505678-cf99a9908e03?w=600&auto=format&fit=crop&q=60", model: "/models/king_size.glb" },
+    { name: "Wooden Bed", category: "beds", rating: "★★★★★", price: "VNĐ2496700", originalPrice: "VNĐ3749200", image: "https://images.unsplash.com/photo-1688384452844-8364c3e2fc28?w=600&auto=format&fit=crop&q=60", model: "/models/wooden_bed.glb" },
+    { name: "Sleek Bed", category: "beds", rating: "★★★★★", price: "VNĐ2663700", originalPrice: "VNĐ3081200", image: "https://plus.unsplash.com/premium_photo-1670076515907-2736a3492f23?w=600&auto=format&fit=crop&q=60", model: "/models/sleek_bed.glb" },
+    { name: "Minimalist Bed", category: "beds", rating: "★★★★☆", price: "VNĐ2329700", originalPrice: "VNĐ2747200", image: "https://plus.unsplash.com/premium_photo-1671269943825-e45b177add8f?w=600&auto=format&fit=crop&q=60", model: "/models/minimalist_bed.glb" },
+    { name: "Leather Sofa", category: "sofas", rating: "★★★★★", price: "VNĐ4166700", originalPrice: "VNĐ4584200", image: "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?w=600&auto=format&fit=crop&q=60", model: "/models//chesterfield-sofa.glb" },
+    { name: "Modern Sofa", category: "sofas", rating: "★★★★★", price: "VNĐ3331700", originalPrice: "VNĐ3749200", image: "https://images.unsplash.com/photo-1698936061086-2bf99c7b9fc5?w=600&auto=format&fit=crop&q=60", model: "/models/low_poly_modern_sofa_free_model.glb" },
+    { name: "Velvet Sofa", category: "sofas", rating: "★★★★★", price: "VNĐ3832700", originalPrice: "VNĐ4166700", image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=600&auto=format&fit=crop&q=60", model: "/models/green_velvet_three_seatersofa.glb" },
+    { name: "Compact Sofa", category: "sofas", rating: "★★★★☆", price: "VNĐ2916700", originalPrice: "VNĐ3248200", image: "https://images.unsplash.com/photo-1647221598498-245f6ed6c720?w=600&auto=format&fit=crop&q=60", model: "/models/compact_sofa.glb" },
+    { name: "L-Shaped Sofa", category: "sofas", rating: "★★★★★", price: "VNĐ5001700", originalPrice: "VNĐ5419200", image: "https://plus.unsplash.com/premium_photo-1692130314358-30f911957d7f?w=600&auto=format&fit=crop&q=60", model: "/models/L-shape.glb" },
+    { name: "Coffee Table", category: "tables", rating: "★★★★★", price: "VNĐ1076700", originalPrice: "VNĐ1244200", image: "https://images.unsplash.com/photo-1461418559055-6f020c5a91e7?w=600&auto=format&fit=crop&q=60", model: "/models/bar_table.glb" },
+    { name: "Dining Table", category: "tables", rating: "★★★★★", price: "VNĐ2496700", originalPrice: "VNĐ2915200", image: "https://images.unsplash.com/photo-1656470176663-1f5ed664e539?w=600&auto=format&fit=crop&q=60", model: "/models/simple_dining_table.glb" },
+    { name: "Side Table", category: "tables", rating: "★★★★☆", price: "VNĐ659700", originalPrice: "VNĐ826700", image: "https://images.unsplash.com/photo-1494949385013-8b57482a0e4f?w=600&auto=format&fit=crop&q=60", model: "/models/ikea_lack_side_table_55x55x45.glb" },
+    { name: "Round Table", category: "tables", rating: "★★★★☆", price: "VNĐ1244200", originalPrice: "VNĐ1411200", image: "https://plus.unsplash.com/premium_photo-1670869816894-e020bc93b279?w=600&auto=format&fit=crop&q=60", model: "/models/round_table.glb" },
+    { name: "Glass Table", category: "tables", rating: "★★★★★", price: "VNĐ1661700", originalPrice: "VNĐ1912200", image: "https://images.unsplash.com/photo-1563146413-d915a569d6b1?w=600&auto=format&fit=crop&q=60", model: "/models/round_glass_table.glb" }
   ];
 
   const handleARView = (product) => {
@@ -134,7 +134,6 @@ const Category = () => {
                 {selectedProduct.price}{" "}
                 <span className="original-price">{selectedProduct.originalPrice}</span>
               </p>
-              <p>{selectedProduct.description || "Hurry Up! Sale is Over in 2-Hours."}</p>
             </div>
             <model-viewer
               src={selectedProduct.model}
